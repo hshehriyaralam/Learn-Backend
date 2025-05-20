f30679719ce494cfb9dd5b28c8b0e187ba9e0d07
hshehriyaralam <hshehriyaralam@gmail.com>
2025-05-20 22:38:58 +0500

    learn multer & cloudinary

---
+++ 05-Setup Backend/src/Utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from 'fs'
+
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.API_SECRET_KEY
+});
+
+const uploadOnCloudinary = async (localFilePath)  => {
+    try{
+        if(!localFilePath) return null
+        //uplaod file cloudinary
+       const response =  await  cloudinary.uploader.upload(localFilePath, {
+            resource_type : "auto"
+        })
+        // file has been uploaded
+        console.log("File Uploaded on Cloudinary",response.url)
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file 
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
+
+
+
